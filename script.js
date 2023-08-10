69d8986b2dc01bbca50ef68c9a8399525d6c1ff6
Brian <wamwangimathenge@gmail.com>
2023-08-10 14:56:53 +0300

    Improve the class using default parameters
    This commit gives default parameters thus improving the class function.

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ console.log(fail.calculate());
 
 // var pass = new NoDefaultParams(1, 2, 3, "pass", true)
 // console.log(pass.calculate());
+
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+        this.num1 = num1
+        this.num2 = num2
+        this.num3 = num3
+        this.string1 = string1
+        this.bool1 = bool1
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+
+var better = new WithDefaultParams()
+console.log(better.calculate());
